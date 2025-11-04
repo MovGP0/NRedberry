b7e6d4ec0879248f1ab4af8f692a5bd7563bda23
Johann Dirry <johann.dirry@microsea.at>
2025-11-04 18:12:14 +0100

    porting NoLiftingException

--- NRedberry.JAS/Ufd/NoLiftingException.cs
+++ NRedberry.JAS/Ufd/NoLiftingException.cs
@@ -1,15 +1,24 @@
-namespace NRedberry.Core.Transformations.Factor.Jasfactor.Edu.Jas.Ufd;
+﻿namespace NRedberry.Core.Transformations.Factor.Jasfactor.Edu.Jas.Ufd;
 
 /// <summary>
-/// Non existing Hensel lifting. Exception to be thrown when a valid Hensel lifting cannot be constructed.
+/// Non-existing Hensel lifting. Exception to be thrown when a valid Hensel lifting cannot be constructed.
 /// </summary>
 /// <remarks>
 /// Original Java file: cc.redberry.core.transformations.factor.jasfactor.edu.jas.ufd.NoLiftingException
 /// </remarks>
 public class NoLiftingException : Exception
 {
+    public NoLiftingException()
+    {
+    }
+
     public NoLiftingException(string message)
         : base(message)
+    {
+    }
+
+    public NoLiftingException(string? message, Exception? innerException)
+        : base(message, innerException)
     {
     }
 }
