8ca61e54572237beb6f4458e9589689d5aa160eb
Johann Dirry <movgp0@gmail.com>
2025-11-09 18:13:14 +0100

    simplifying ArrayUtils.Swap methods

--- NRedberry.Core.Utils/ArraysUtils.Swap.cs
+++ NRedberry.Core.Utils/ArraysUtils.Swap.cs
@@ -2,49 +2,6 @@ namespace NRedberry.Core.Utils;
 
 public static partial class ArraysUtils
 {
-    private static void swap(int[] x, int a, int b, int[] coSort)
-    {
-        swap(x, a, b);
-        swap(coSort, a, b);
-    }
-
-    /// <summary>
-    /// Swaps x[a] with x[b].
-    /// </summary>
-    /// <param name="x"></param>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    private static void swap(int[] x, int a, int b)
-    {
-        (x[a], x[b]) = (x[b], x[a]);
-    }
-
-    private static void vecswap(int[] x, int a, int b, int n, int[] coSort)
-    {
-        for (int i = 0; i < n; i++, a++, b++)
-            swap(x, a, b, coSort);
-    }
-
-    private static void swap(int[] x, int a, int b, long[] coSort)
-    {
-        swap(x, a, b);
-        swap(coSort, a, b);
-    }
-
-    /**
-         * Swaps x[a] with x[b].
-         */
-    private static void swap(long[] x, int a, int b)
-    {
-        (x[a], x[b]) = (x[b], x[a]);
-    }
-
-    private static void vecswap(int[] x, int a, int b, int n, long[] coSort)
-    {
-        for (int i = 0; i < n; i++, a++, b++)
-            swap(x, a, b, coSort);
-    }
-
     private static void Swap(int[] x, int a, int b, int[] coSort)
     {
         Swap(x, a, b);
@@ -59,7 +16,9 @@ public static partial class ArraysUtils
     private static void Vecswap(int[] x, int a, int b, int n, int[] coSort)
     {
         for (int i = 0; i < n; i++, a++, b++)
+        {
             Swap(x, a, b, coSort);
+        }
     }
 
     private static void Swap(long[] x, int a, int b, long[] coSort)
@@ -71,7 +30,9 @@ public static partial class ArraysUtils
     private static void Vecswap(long[] x, int a, int b, int n, long[] coSort)
     {
         for (int i = 0; i < n; i++, a++, b++)
+        {
             Swap(x, a, b, coSort);
+        }
     }
 
     private static void Swap(int[] x, int a, int b, long[] coSort)
@@ -88,7 +49,9 @@ public static partial class ArraysUtils
     private static void Vecswap(int[] x, int a, int b, int n, long[] coSort)
     {
         for (int i = 0; i < n; i++, a++, b++)
+        {
             Swap(x, a, b, coSort);
+        }
     }
 
     private static void Swap<T>(T[] x, int a, int b, object[] coSort) where T : IComparable<T>
@@ -105,7 +68,9 @@ public static partial class ArraysUtils
     private static void Vecswap<T>(T[] x, int a, int b, int n, object[] coSort) where T : IComparable<T>
     {
         for (int i = 0; i < n; i++, a++, b++)
+        {
             Swap(x, a, b, coSort);
+        }
     }
 
     private static void Swap<T>(T[] x, int a, int b, int[] coSort) where T : IComparable<T>
@@ -117,6 +82,8 @@ public static partial class ArraysUtils
     private static void Vecswap<T>(T[] x, int a, int b, int n, int[] coSort) where T : IComparable<T>
     {
         for (int i = 0; i < n; i++, a++, b++)
+        {
             Swap(x, a, b, coSort);
+        }
     }
 }
