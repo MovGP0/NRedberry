36d7d16b001b66df46e76ce7b4f1a3b850f6d4b8
Johann Dirry <movgp0@gmail.com>
2025-11-23 17:04:21 +0100

    removing EmptyIterator

--- NRedberry.Core.Utils/EmptyIterator.cs
+++ NRedberry.Core.Utils/EmptyIterator.cs
@@ -1,35 +1,7 @@
-using System.Collections;
-
 namespace NRedberry.Core.Utils;
 
 /// <summary>
 /// Skeleton port of cc.redberry.core.utils.EmptyIterator.
 /// </summary>
 [Obsolete("Use Enumerable.Empty<T>() instead", true)]
-public sealed class EmptyIterator<T> : IEnumerator<T>
-{
-    public static EmptyIterator<T> Instance { get; } = new();
-
-    private EmptyIterator()
-    {
-    }
-
-    public bool MoveNext()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Reset()
-    {
-        throw new NotImplementedException();
-    }
-
-    public T Current => throw new NotImplementedException();
-
-    object IEnumerator.Current => Current!;
-
-    public void Dispose()
-    {
-        throw new NotImplementedException();
-    }
-}
+public sealed class EmptyIterator<T>;
