8a90f2034fbdc9125b52d067f824ea6eac0cd131
Johann Dirry <movgp0@gmail.com>
2025-11-23 17:04:52 +0100

    removing ArrayIterator

--- NRedberry.Core.Utils/ArrayIterator.cs
+++ NRedberry.Core.Utils/ArrayIterator.cs
@@ -1,36 +1,7 @@
-using System.Collections;
-
 namespace NRedberry.Core.Utils;
 
 /// <summary>
 /// Skeleton port of cc.redberry.core.utils.ArrayIterator.
 /// </summary>
 [Obsolete("Arrays can be enumerated directly in C#", true)]
-public sealed class ArrayIterator<T> : IEnumerator<T>
-{
-    private readonly T[] array = [];
-    private int index = -1;
-
-    public ArrayIterator(T[] array)
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool MoveNext()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Reset()
-    {
-        throw new NotImplementedException();
-    }
-
-    public T Current => throw new NotImplementedException();
-
-    object IEnumerator.Current => Current!;
-
-    public void Dispose()
-    {
-    }
-}
+public sealed class ArrayIterator<T>;
