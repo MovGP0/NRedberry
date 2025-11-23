87e46dedacd69db680c5644a26283ea9027f30f6
Johann Dirry <movgp0@gmail.com>
2025-11-23 17:06:34 +0100

    removing IntComparator

--- NRedberry.Core.Utils/IntComparator.cs
+++ NRedberry.Core.Utils/IntComparator.cs
@@ -3,15 +3,8 @@ namespace NRedberry.Core.Utils;
 /// <summary>
 /// Skeleton port of cc.redberry.core.utils.IntComparator.
 /// </summary>
-public interface IntComparator
-{
-    int Compare(int first, int second);
-}
+[Obsolete("Use IComparator<int> instead", true)]
+public interface IntComparator;
 
-/// <summary>
-/// Provides helper members for <see cref="IntComparator"/>.
-/// </summary>
-public static class IntComparators
-{
-    public static IntComparator Default => throw new NotImplementedException();
-}
+[Obsolete("Use IComparator<int> instead", true)]
+public static class IntComparators;
